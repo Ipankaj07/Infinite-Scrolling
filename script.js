fc57a5140968cbccc012f7e3f12a35002a30a953
Pankaj Raj <praj4936@gmail.com>
2022-02-05 20:51:57 +0530

    script

---
+++ script.js
@@ -0,0 +1,45 @@
+let content = document.getElementById('content');
+
+let items = [];
+
+for (let i = 1; i <= 25; i++) {
+    items.push('<div class="li" >Item ' + i + '</div>');
+}
+
+// console.log(`items => ` + items);
+
+content.innerHTML = items.join('');
+
+let scroll = function () {
+    let rect = content.getBoundingClientRect();
+    // console.log(rect);
+
+    let itemCount = content.getElementsByClassName('li').length;
+
+    if (rect.bottom <= window.innerHeight) {
+        let newItems = [];
+        for (let i = 1; i <= 25; i++) {
+            newItems.push('<div class="li">Item ' + (itemCount + i) + '</div>');
+        }
+        content.innerHTML += newItems.join('');
+    }
+
+};
+
+window.addEventListener('scroll', scroll);
+
+let scrollTopBtn = document.getElementById('scroll-top-btn');
+
+let scrollTop = function () {
+    if (window.pageYOffset > 150) {
+        scrollTopBtn.style.display = 'block';
+    } else {
+        scrollTopBtn.style.display = 'none';
+    }
+};
+
+window.addEventListener('scroll', scrollTop);
+
+scrollTopBtn.addEventListener('click', function () {
+    window.scrollTo(0, 0);
+});
